81ffa59c28427952975a18f229a599e7610f349a
Iman <iman.osama2000@gmail.com>
2024-04-16 14:03:54 +0200

    [feat]: pagination buttons

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,11 +1,52 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/states/app.state';
+import { nextPage, previousPage, setPage } from 'src/app/states/page/page.actions';
+import { selectPage } from 'src/app/states/page/page.selector';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.scss']
+  styleUrls: ['./pagination.component.scss'],
 })
 export class PaginationComponent {
-  currentPage = 3;
-  @Input() pages:number[] = [1, 2, 3, 4]
+  @Input() pages: number[] = [1, 2, 3, 4]
+  @ViewChild('nextButton') nextButton!: ElementRef<HTMLButtonElement>;
+  @ViewChild('previousButton') previousButton!: ElementRef;
+  page$: Observable<number>;
+
+
+  constructor(private store: Store<AppState>) {
+    this.page$ = this.store.select(selectPage);
+  }
+  ngAfterViewInit() {
+    this.handlePageLimits();
+  }
+  handlePageLimits() {
+    this.page$.subscribe(page => {
+      if (page === this.pages[this.pages.length - 1]) {
+        this.nextButton.nativeElement.classList.add('disabled');
+      }
+      else {
+        this.nextButton.nativeElement.classList.remove('disabled');
+      }
+      if (page === this.pages[0]) {
+        this.previousButton.nativeElement.classList.add('disabled');
+      }
+      else {
+        this.previousButton.nativeElement.classList.remove('disabled');
+      }
+    });
+  }
+
+  nextPage() {
+    this.store.dispatch(nextPage());
+  }
+  previousPage() {
+    this.store.dispatch(previousPage());
+  }
+  setPage(page: number) {
+    this.store.dispatch(setPage(page));
+  }
 }
