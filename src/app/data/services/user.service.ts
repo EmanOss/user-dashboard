bb27eaf31039d7e243c47284f62690ab9de0510c
Iman <iman.osama2000@gmail.com>
2024-04-17 17:57:17 +0200

    [feat]: caching api responses

--- src/app/data/services/user.service.ts
+++ src/app/data/services/user.service.ts
@@ -1,28 +1,55 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { User } from '../interfaces/user';
 import { environment } from 'src/environments/environment.development';
+import { ApiCacheService } from 'src/app/core/services/api-cache.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private apiCacheService: ApiCacheService) { }
 
   getAllUsers(page: number): Observable<User[]> {
-    console.log('getting all users');
+    // console.log('getting all users');
     const url = `${environment.apiUserEndpoint}?page=${page}`;
-    return this.http.get<{ data: User[] }>(url).pipe(
-      map(response => response.data)
+    return this.apiCacheService.get<User[]>(url).pipe(
+      switchMap(cachedData => {
+        if (cachedData) {
+          return of(cachedData);
+        } else {
+          return this.http.get<{ data: User[] }>(url).pipe(
+            map(response => response.data),
+            switchMap(data => {
+              this.apiCacheService.set(url, data);
+              return of(data);
+            })
+          );
+        }
+      })
     );
   }
+
   getUserById(userId: string): Observable<User> {
-    console.log('userId', userId);
+    // console.log('userId', userId);
     const url = `${environment.apiUserEndpoint}/${userId}`;
-    return this.http.get<{ data: User }>(url).pipe(
-      map(response => response.data)
+    return this.apiCacheService.get<User>(url).pipe(
+      switchMap(cachedData => {
+        if (cachedData) {
+          return of(cachedData);
+        } else {
+          return this.http.get<{ data: User }>(url).pipe(
+            map(response => response.data),
+            switchMap(data => {
+              this.apiCacheService.set(url, data);
+              return of(data);
+            })
+          );
+        }
+      })
     );
   }
 }
